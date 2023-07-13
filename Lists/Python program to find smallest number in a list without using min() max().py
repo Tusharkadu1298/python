2c120cb0dea083475d5abfa9f7ce95f0575a708c
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-07-13 19:40:38 +0530

    Create Python program to find smallest number in a list without using min() max().py

---
+++ Lists/Python program to find smallest number in a list without using min() max().py
@@ -0,0 +1,8 @@
+# 9.  Python program to find smallest number in a list without using min()
+# 10. Python program to find largest number in a list without using max()
+
+list= [2514,365,142,86,4225,155,185,51,52485,5552]
+list.sort()
+print(list)
+print(list[0])
+print(list[-1])
