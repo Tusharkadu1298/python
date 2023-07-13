436e6702694f2f3ebb936b7468877c714c053325
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-07-13 19:38:02 +0530

    Create 3. write a program to find length of list without using len.py

---
+++ Lists/3. write a program to find length of list without using len.py
@@ -0,0 +1,7 @@
+# 3. write a program to find length of list without using len()
+
+list = [10,20,30,40,50,60,70,80,90,100,10,20,30,40,50,60,70,80,90,100,10,20,30,40,50,60,70,80]
+len = 0
+for i in list:
+    len += 1
+print(len)
