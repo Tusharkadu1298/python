2793124ff40d3a8ae0594bd852f79f1e9a5cf21f
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-07-22 17:38:39 +0530

    Add files via upload

---
+++ Dictionary/2. write a python program to find the mean of the values.py
@@ -0,0 +1,9 @@
+# 2. write a python program to find the mean of the values
+
+
+a = {"a":20,"b":30,"c":60}
+print(len(a))
+sum = 0
+for i in a.values():
+    sum += i
+print((sum)/3)
---
+++ Dictionary/3. write a program to update the name to _David_ in the given dictionary.py
@@ -0,0 +1,7 @@
+# 3. write a program to update the name to "David" in the given dictionary
+# a = {"name":"john","age":23,"gender":"male"}
+
+
+a = {"name":"john","age":23,"gender":"male"}
+a['name'] = 'david'
+print(a)
