6a3f6bdf5065ffde39d3d62184d1608e47c2232d
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-07-22 17:29:51 +0530

    4.  Write a python program to check if element exists in list.py

---
+++ Lists/4.  Write a python program to check if element exists in list.py
@@ -0,0 +1,9 @@
+# 4.  Write a python program to check if element exists in list
+
+
+a = [10,20,30,33,40,47,50,53,70,71,80,50,90,50,93,95,50,97,100]
+i = 50
+if i in a:
+    print("yes it exist")
+else:
+    print("no it doesn't exist")
