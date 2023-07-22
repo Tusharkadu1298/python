b345ea633d4a3f7c7e3a07cb82b15b9c7bab3650
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-07-22 17:32:50 +0530

    7.  Python Program to find sum and average of List in Python.py

---
+++ Lists/7.  Python Program to find sum and average of List in Python.py
@@ -0,0 +1,22 @@
+# 7.  Python Program to find sum and average of List in Python
+  
+list = [10,20,30,40,50,60,70,80,90,100]
+add = sum(list)
+print(add)
+average = sum(list)/len(list)
+print(average)
+
+
+# using for loop
+sum = 0
+for i in l:
+    sum += i
+print(sum)
+ 
+len = 0
+for i in l:
+    len += 1
+print(len)
+
+c = sum/len
+print(c)
