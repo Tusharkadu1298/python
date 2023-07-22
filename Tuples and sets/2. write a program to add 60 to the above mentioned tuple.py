0bb70e6bf78e96988aceee4674f2eb0f4c58bd56
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-07-22 17:43:28 +0530

    Add files via upload

---
+++ Tuples and sets/2. write a program to add 60 to the above mentioned tuple.py
@@ -0,0 +1,11 @@
+# a = (23,44,24,33,66,45)
+# 2. write a program to add 60 to the above mentioned tuple.
+
+
+a = (23,44,24,33,66,45)
+a = list(a)
+print(a)
+a.append(60)
+print(a)
+a = tuple(a)
+print(a)
---
+++ Tuples and sets/5. Python program to find the difference between two lists.py
@@ -0,0 +1,6 @@
+#  5. Python program to find the difference between two lists
+
+a = {1,2,4,6}
+b = {2,3,6}
+print(a.symmetric_difference(b))
+print(a^b)
